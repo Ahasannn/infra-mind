3b8c5c2d791f0da0157b5673f90e710cde62a219
Jiaqi Xue <ji757406.ucf@c1003a-s15.ufhpc>
2026-01-25 03:28:31 -0500

    Updated - HumanEval

--- scripts/mas_test_mbpp.sh
+++ scripts/mas_test_mbpp.sh
@@ -9,9 +9,9 @@ CHECKPOINT_DIR="${BLUE_STORAGE}/checkpoints/mas_router"
 export MBPP_DATASET_PATH="${BLUE_STORAGE}/datasets/mbpp/full"
 
 python Experiments/run_mbpp.py \
-  --test_limit 50 \
-  --concurrency 10 \
-  --arrival-rate 5 15 20\
+  --test_limit 300 \
+  --concurrency 70 \
+  --arrival-rate 10 30 50 70 100 150 200\
   --max_agent 5 \
   --checkpoint "${CHECKPOINT_DIR}/mas_mbpp_train_full_300.pth" \
   --epochs 0
